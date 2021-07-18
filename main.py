8f30ff4f536a969957da58bc5b2107520578436d
janardhan <janardhankarravula@gmail.com>
2021-07-18 06:02:25 +0530

    procfile added

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ app = Flask(__name__)
 # db config
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # bootstrap config
