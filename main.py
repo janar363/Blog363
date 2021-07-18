13ddfd17e30df7bfa92423d28d5447ed22aae506
janardhan <janardhankarravula@gmail.com>
2021-07-18 07:28:58 +0530

    db updated

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ from flask_login import UserMixin, LoginManager, login_user, logout_user, login_
 app = Flask(__name__)
 
 # db config
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
